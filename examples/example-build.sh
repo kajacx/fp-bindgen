16159b0d2dbf9e83f30434644e53b46449172656
kajacx <kajacx@gmail.com>
2023-01-30 18:08:27 +0100

    Add stages print to build script

--- examples/example-build.sh
+++ examples/example-build.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/sh
 
+echo "Generating protocol..." && \
 cd example-protocol && cargo run && cd .. && \
+echo "Protocol generated, building plugin..." && \
 cd example-plugin && cargo build && cd .. && \
+echo "Plugin built, copying files..." && \
 cp example-protocol/bindings/rust-wasmer-runtime/bindings.rs example-rust-wasmer-runtime/src/spec/bindings.rs && \
 cp example-protocol/bindings/rust-wasmer-runtime/types.rs example-rust-wasmer-runtime/src/spec/types.rs && \
 cp example-protocol/bindings/rust-wasmer-wasi-runtime/bindings.rs example-rust-wasmer-runtime/src/wasi_spec/bindings.rs && \
 cp example-protocol/bindings/rust-wasmer-wasi-runtime/types.rs example-rust-wasmer-runtime/src/wasi_spec/types.rs && \
-cd example-rust-wasmer-runtime && cargo test
+echo "Files copied, running rntime test..." && \
+cd example-rust-wasmer-runtime && cargo test && \
+echo "All done."
